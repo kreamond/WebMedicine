37273bde1405450f25473e2036b42f7b24422734
kreamond <kreamond@mail.ru>
2015-10-28 19:30:27 +0300

    Класс User

---
+++ WebMedicine/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMedicine.Models
+{
+    public class User
+    {
+        //ID
+        public int Id { get; set; }
+        
+        //Фамилия Имя Отчество
+        [Required]
+        [Display(Name = "Фамилия Имя Отчество")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Name { get; set; }
+        
+        //Логин
+        [Required]
+        [Display(Name ="Логин")]
+        [MaxLength(50, ErrorMessage ="Превышена максимальная длина записи")]
+        public string Login { get; set; }
+
+        //Пароль
+        [Required]
+        [Display(Name ="Пароль")]
+        [MaxLength(50, ErrorMessage ="Превышена максимальная длина записи")]
+        public string Password { get; set; }
+
+        //Дожность
+        [Display(Name ="Должность")]
+        [MaxLength(50, ErrorMessage ="Превышена максимальная длина записи")]
+        public string Position { get; set; }
+
+        //Отдел
+        [Display(Name ="Отдел")]
+        public int? DepartmentId { get; set; }
+
+        //Статус
+        [Required]
+        [Display(Name ="Статус")]
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+
+    }
+}
